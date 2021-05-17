08d74c6bb86f794bb83d62cf301adc405eeff9db
Sriram B S <bs_sriram@hotmail.com>
2021-05-17 10:38:27 +0530

    Device motion added

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@ closeMenu.addEventListener('click', ()=>{
 
 /*===== MOUSEMOVE HOME IMG =====*/
 document.addEventListener('mousemove', move);
-document.addEventListener('touchmove', move);
+document.addEventListener('devicemotion', move);
 function move(e){
     this.querySelectorAll('.move').forEach(layer =>{
         const speed = layer.getAttribute('data-speed')
