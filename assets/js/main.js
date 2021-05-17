0b6dfc801971845537459381b19e32136f5fe1ce
Sriram B S <bs_sriram@hotmail.com>
2021-05-17 10:43:54 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,13 +15,23 @@ closeMenu.addEventListener('click', ()=>{
 
 /*===== MOUSEMOVE HOME IMG =====*/
 document.addEventListener('mousemove', move);
-document.addEventListener('devicemotion', move);
+document.addEventListener('devicemotion', deviceMove);
 function move(e){
     this.querySelectorAll('.move').forEach(layer =>{
         const speed = layer.getAttribute('data-speed')
 
         const x = (window.innerWidth - e.pageX*speed)/120
         const y = (window.innerHeight - e.pageY*speed)/120
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
+function deviceMove(e.){
+    this.querySelectorAll('.move').forEach(layer =>{
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.acceleration.x)/120
+        const y = (window.innerHeight - e.acceleration.y)/120
 
         layer.style.transform = `translateX(${x}px) translateY(${y}px)`
     })
